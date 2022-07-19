782ffcd629750b21e46c473db4ff9f28c7c3de8f
Alcid <alcidvincent@gmail.com>
2022-07-20 01:51:13 +0800

    Add formidable, FormData, and axios dependencies and update addStudent to upload image

--- controllers/studentsControllers.ts
+++ controllers/studentsControllers.ts
@@ -1,6 +1,10 @@
 import { AppDataSource } from "../database/data-source";
 import { Student } from "../database/entity/Student";
 import { StudentData } from "../models/students";
+import formidable from 'formidable';
+import FormData from "form-data";
+import axios from "axios";
+import fs from "fs";
 
 export const getAllStudents = async (req, res) => {
     const studentRepository = AppDataSource.getRepository(Student);
@@ -16,17 +20,42 @@ export const getStudentById = async (req, res) => {
     return res.status(200).send({ message: 'Request successful', data: student});
 }
 
-export const addStudent = async (req, res) => {
-    const body: StudentData = req.body;
-    const newStudent = new Student()
-    newStudent.name = body.name
-    newStudent.birthday = body.birthday
-    newStudent.year_level = parseInt(body.year_level)
-    newStudent.address = body.address
-    newStudent.contact_number = body.contact_number
-    const studentRepository = AppDataSource.getRepository(Student);
-    await studentRepository.save(newStudent)
-    return res.status(201).send({ message: 'Student successfully added', data: newStudent});
+export const addStudent = async (req, res, next) => {
+    const form = formidable({});
+        form.parse(req, async (err, fields, files) => {
+            if (err) {
+                next(err);
+                return;
+            }
+            const newStudent = new Student()
+            newStudent.name = fields.name
+            newStudent.birthday = fields.birthday
+            newStudent.year_level = +fields.year_level
+            newStudent.address = fields.address
+            newStudent.contact_number = fields.contact_number
+            // save image to cloudinary
+            try {
+                const form = new FormData();
+                form.append('file', fs.createReadStream(files.picture.filepath))
+                form.append('upload_preset', 'upreset')
+                form.append('public_id', files.picture.newFilename)
+                form.append('api_key', process.env.CLOUDINARY_API_KEY)
+                const response: any = await axios.post(
+                    `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload`, 
+                    form, 
+                    {
+                        headers: { "Content-Type": "multipart/form-data" }
+                    }
+                )
+
+                newStudent.picture = response.data.url
+                const studentRepository = AppDataSource.getRepository(Student);
+                await studentRepository.save(newStudent)
+                return res.status(201).send({ message: 'Student successfully added', data: newStudent});
+            } catch (error) {
+                return res.status(500).send({ message: "Error Encountered", error })   
+            }
+        });
 }
 
 export const patchStudentInfo = async (req, res) => {
--- database/entity/Student.ts
+++ database/entity/Student.ts
@@ -25,4 +25,9 @@ export class Student {
 
     @Column()
     contact_number: string
+
+    @Column({
+        nullable: true
+    })
+    picture: string
 }
